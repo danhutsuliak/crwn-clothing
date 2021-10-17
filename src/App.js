2819e5bb9d3bdb6bf172ceef9f50ba31e1519048
unknown <danguts23@gmail.com>
2021-10-17 22:33:04 +0300

    added addItem and removeItem functionality to checkout item component

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import CheckoutPage from './pages/checkout/checkout.components';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
-// import { selectCartHidden } from "./redux/cart/cart.selectors";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
